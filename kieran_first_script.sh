8dcb88549f4bb3348b6bf3838af837d72865a355
aterrell <aterrell@monsooncommerce.com>
2013-05-20 19:24:54 -0700

    my first commit

---
+++ kieran_first_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function juliet {
+    echo "meaaaoooowwwww"
+    echo "i'm a kitty, and my poop smells bad"
+}
+
+function amber {
+    echo GET OUT POOP MONSTER!  
+}
+
+function aaron {
+ echo shes just furry not fat
+}
---
+++ kierans_second_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source ./kieran_first_script.sh
+
+juliet
+amber
+aaron
